55ce9b173dba3a056da812f5f24877067c525dc8
supritkumar shah <suprit7@gmail.com>
2016-10-12 18:56:06 -0400

    add components

---
+++ src/components/Map.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Map extends Component{
+  render(){
+    return(
+      <div>This is the map Component </div>
+      )
+  }
+}
+
+export default Map;
